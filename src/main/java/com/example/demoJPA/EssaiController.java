8b7efa4573ae5759dfb025fcf30a1e8b0bed93be
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-02-15 20:07:17 +0100

    demoJPA commit sur ajout du premier WebService nommée EssaiController sur ajout, modification, suppression d'une données sur la table Car de la base de données.

--- src/main/java/com/example/demoJPA/EssaiController.java
+++ src/main/java/com/example/demoJPA/EssaiController.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class EssaiController {
     @Autowired
     private CarRepository carrepo;
 
     //Regarder tous les voitures au lancement de l'application la bdd est vide alors tout d'abord créer une bdd à l aide
-    //de http://localhost:8088/voitures/1122AA/feri/24000 par exemple avant de lancer http://localhost:8088/voitures
+    //de http://localhost:8088/voitures/create/1122AB/feri/2400 par exemple avant de lancer http://localhost:8088/voitures
     @GetMapping("/voitures")
     public List<Car> findAll() {
 
@@ -25,7 +27,8 @@ public class EssaiController {
 
         return cars;
     }
-    @GetMapping("/voitures/{plateNumber}")
+    //Pour supprimer par son numero de serie
+    @GetMapping("/voitures/delete/{plateNumber}")
     public String Car_deleteById(@PathVariable(value = "plateNumber") String p_plateNumber) {
         String success;
         if(carrepo != null){
@@ -37,7 +40,20 @@ public class EssaiController {
         }
         return success;
     }
-    @GetMapping("/voitures/{num_serie}/{marque}/{prix}")
+    //Pour voir par son numero de serie
+    @GetMapping("/voitures/{plateNumber}")
+    public Optional<Car> Car_SeeById(@PathVariable(value = "plateNumber") String p_plateNumber) {
+        var it = carrepo.findById(p_plateNumber);
+        if(it!=null){
+            System.out.println("La voiture existe et trouvé dans le navigateur web!");
+        }
+        else{
+            System.out.println("La voiture n'existe pas donc la valeur trouvé est null");
+        }
+        return it;
+    }
+    //Pour créer une voiture
+    @GetMapping("/voitures/create/{num_serie}/{marque}/{prix}")
     public String Car_Create_or_Save(@PathVariable(value = "num_serie") String p_plateNumber,
                              @PathVariable(value = "marque") String p_brand, @PathVariable(value = "prix") int p_price) {
         String success;
