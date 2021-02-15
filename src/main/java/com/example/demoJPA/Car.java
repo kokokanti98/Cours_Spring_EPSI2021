269b772050b55f910fe92ce5c8517eeff62b35c2
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-02-15 18:14:47 +0100

    Premier commit sur demoJPA

---
+++ src/main/java/com/example/demoJPA/Car.java
@@ -0,0 +1,49 @@
+package com.example.demoJPA;
+
+import javax.persistence.Id;
+
+public class Car {
+    private String plateNumber;
+    private String brand;
+    private int price;
+
+    public Car() {
+        super();
+    }
+
+    public Car(String plateNumber, String brand, int price) {
+        super();
+        this.plateNumber = plateNumber;
+        this.brand = brand;
+        this.price = price;
+    }
+    @Id
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [plateNumber=" + plateNumber + ", brand=" + brand + ", price=" + price + "]";
+    }
+}
